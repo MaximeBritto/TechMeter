1d230e81e12bff6ede518725ff59cbc27ef17540
Maxime <maxime.britto@edu.devinci.fr>
2023-12-18 14:25:47 +0100

    Vite upgrade

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,9 @@ function App() {
 
   return (
     <>
+
       <div>
+        
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
