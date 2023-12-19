f724f3c4bd6dde6807707b3b2e849130ad166964
RyomaQ <ryoma1@hotmail.fr>
2023-12-19 14:39:34 +0100

    file rename

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import SliderContainer from "./composant/SliderContainer.jsx";
-import Bareme from "./composant/Bareme.jsx";
+import Bareme from "./composant/bareme.jsx";
 import "./index.css";
 import techmeterLogo from "./assets/techmeter-logo.svg"; 
 
