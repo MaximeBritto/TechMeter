e970adb58de40db54cd9e0d8696af248074ac38e
Joanne <90553540+joannemas@users.noreply.github.com>
2023-12-19 14:19:57 +0100

    ✨ Add Accordion

--- src/composant/SliderContainer.jsx
+++ src/composant/SliderContainer.jsx
@@ -1,33 +1,50 @@
 import { Box, Slider, Stack } from '@mui/material';
 import reactLogo from '../assets/react.svg';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
-function SliderContainer({name}) {
+function SliderContainer({name, desc}) {
   
   function valuetext(value) {
     return `${value}°C`;
   }
   return (
     <>
-<Stack style={{ border: '1px solid #ccc', width: '100%' }}>
-  <Box style={{ display: 'flex', padding: '2rem 3rem', gap: '2rem', alignItems: 'center' }}>
-    <Box style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      <p style={{width: "15rem",  fontSize: "1.2rem"}}>{name}</p>
-      <img src={reactLogo} alt="react logo" />
-    </Box>
-    <Slider
-      aria-label="Temperature"
-      defaultValue={0}
-      getAriaValueText={valuetext}
-      valueLabelDisplay="auto"
-      step={1}
-      marks
-      min={0}
-      max={6}
-    />
-  </Box>
-  
-</Stack>
-       
+      <Stack style={{ border: '1px solid #ccc', width: '100%' }}>
+      <Box style={{ display: 'flex', padding: '2rem 3rem', gap: '2rem', alignItems: 'center' }}>
+        <Accordion style={{ backgroundColor: 'transparent', color: 'white'}}>
+          <AccordionSummary expandIcon={<ExpandMoreIcon />} style={{ cursor: 'default' }}>
+            <Box style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginRight: '50px', cursor: 'pointer'}}>
+            <p style={{ width: "15rem", fontSize: "1.2rem" }}>{name}</p>
+              <img src={reactLogo} alt="react logo" />
+              <ExpandMoreIcon style={{ cursor: 'pointer' }} />
+            </Box>
+            <Slider
+              style={{ width: "50vw" }}
+              aria-label="Temperature"
+              defaultValue={0}
+              getAriaValueText={valuetext}
+              valueLabelDisplay="auto"
+              step={1}
+              marks
+              min={0}
+              max={5}
+              onClick={(e) => e.stopPropagation()}
+            />
+          </AccordionSummary>
+
+          <AccordionDetails>
+            <Typography>
+              {desc}
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+      </Box>
+    </Stack>
+
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,8 @@ import { Box } from "@mui/system";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Box style={{ border: "1px solid #ccc", maxWidth: "90rem", padding: "5%"}}>
     <React.StrictMode>
-      <SliderContainer name="Composant"/>
+      <SliderContainer name="Composant" desc="Les composants sont les blocs de construction de l'interface utilisateur dans React.
+Ils peuvent être de deux types : fonctionnels (stateless) ou de classe (stateful)." />
       <SliderContainer name="JSX" />
       <SliderContainer name="Props" />
       <SliderContainer name="Cycle de vie" />
