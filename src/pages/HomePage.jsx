a78806c123857aac8a4c86bcdc1a2dae34c49061
AnthonyDos <47761819+AnthonyDos@users.noreply.github.com>
2023-12-21 12:26:34 +0100

    récupération email user

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import BarChart from '../composant/BarChart.jsx';
 import TechnosList from '../composant/TechnosList.jsx';
 import Data from "../assets/datas/Data.json";
@@ -13,6 +14,7 @@ import { supabase } from "../supabase";
  */
 const HomePage = () => {
     const navigate = useNavigate();
+    const [ email, setEmail ] = useState('');
 
     const buttonStyle = {
         background: "transparent",
@@ -22,6 +24,16 @@ const HomePage = () => {
         height: "20px",
         cursor: "pointer",
     }
+    const checkAuth = async () => {
+        try {
+            const authenticated = await supabase.auth.getUser()
+            setEmail(authenticated?.data?.user?.email);
+           console.log(authenticated);
+        } catch (error) {
+          console.error("Erreur lors de la vérification de l'authentification :", error);
+        }
+    };
+    checkAuth();
 
     const navigateToPage = (name) => {
         navigate('/details/' + name, {replace: true});
@@ -41,9 +53,12 @@ const HomePage = () => {
         <div className='container-homePage' style={{margin: "0 auto", maxWidth: "100rem", padding: "2rem 5%"}}>
             <img src={techmeterLogo} alt="Techmeter logo" style={{width: "15rem", marginBottom: "2rem"}}/>
             <div style={{margin: "2rem 0"}}>
-            <BarChart datas={Data}/>
+                <div style={{marginBottom:"50px"}}>
+                    <h1>{email}</h1>
+                    <span style={{textDecoration: "underline"}}> A4 Fullstack</span>
+                </div>
+                <BarChart datas={Data}/>
             </div>
-            
             <div className='container-technosList'>
                 <TechnosList datas={Data} onClick={navigateToPage} style={buttonStyle}/>
             </div>
